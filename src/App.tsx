b03afad9db0db1a33b73fea3185dc2ee447ac249
DustyFlower <lukashevanatalia1992@gmail.com>
2023-11-29 17:06:14 +0300

    New little tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,175 @@
 import React, {useState} from 'react';
 import './App.css';
-import {FullInput} from './components/FullInput';
-import {Input} from './components/Input';
-import {Button} from './components/Button';
+import {Todolist} from './Todolist';
+import { v1 } from 'uuid';
 
-export type FilterValuesType = 'all' | 'active' | 'completed' | 'three tasks';
-
-function App() {
-    const [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'},
-    ])
-
-    let [title, setTitle] = useState('')
-
-    const addMessage = (title: string) => {
-        let newMessage = {message: title};
-            setMessage([newMessage, ...message])
-    }
-
-    const callBackButtonHandler = () => {
-        addMessage(title)
-        setTitle('')
-    }
-    return (
-        <div className="App">
-{/*            <FullInput addMessage={addMessage}/>*/}
-            <Input setTitle={setTitle} title={title}/>
-            <Button name={'+'} callBack={callBackButtonHandler}/>
-            {message.map((el, index) => {
-                return (
-                    <div key={index}>{el.message}</div>
-                )
-            })}
-        </div>
-    )
-}
-
-
-/*
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-let [tasks, setTasks] = useState([
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: false},
-        {id: 4, title: 'Rest API', isDone: false},
-        {id: 5, title: 'GraphQL', isDone: false},
+    let [tasks, setTasks] = useState([
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "ReactJS", isDone: false },
+        { id: v1(), title: "Rest API", isDone: false },
+        { id: v1(), title: "GraphQL", isDone: false },
     ]);
 
-    function removeTask(id: number) {
-        let filteredTasks = tasks.filter(t => t.id != id);
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id !== id);
         setTasks(filteredTasks);
     }
 
-    const removeAllTasks = () => {
-        setTasks(tasks = [])
+    function addTask(title: string) {
+        let task = { id: v1(), title: title, isDone: false };
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
     }
+
+    let [filter, setFilter] = useState<FilterValuesType>("all");
+
+    let tasksForTodolist = tasks;
+
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+
+
     return (
         <div className="App">
             <Todolist title="What to learn"
-                      tasks={tasks}
-                      removeTask={removeTask}
-/!*                      changeFilter={changeFilter}*!/
-                      removeAllTasks={removeAllTasks}
-            />
+                       tasks={tasksForTodolist}
+                       removeTask={removeTask}
+                       changeFilter={changeFilter}
+                       addTask={addTask} >
+                 <div>
+                     <div>Many intresting information</div>
+                  </div>
+            </Todolist>
         </div>
     );
 }
-*/
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------
+//
+// export type FilterValuesType = "all" | "active" | "completed";
+//
+// function App() {
+//
+//     let [tasks, setTasks] = useState([
+//         {id: v1(), title: "HTML&CSS", isDone: true},
+//         {id: v1(), title: "JS", isDone: true},
+//         {id: v1(), title: "ReactJS", isDone: false},
+//         {id: v1(), title: "Rest API", isDone: false},
+//         {id: v1(), title: "GraphQL", isDone: false},
+//     ]);
+//
+//     function removeTask(id: string) {
+//         let filteredTasks = tasks.filter(t => t.id != id);
+//         setTasks(filteredTasks);
+//     }
+//
+//     function addTask(title: string) {
+//         let task = {id: v1(), title: title, isDone: false};
+//         let newTasks = [task, ...tasks];
+//         setTasks(newTasks);
+//     }
+//
+//     let [filter, setFilter] = useState<FilterValuesType>("all");
+//
+//     let tasksForTodolist = tasks;
+//
+//     if (filter === "active") {
+//         tasksForTodolist = tasks.filter(t => t.isDone === false);
+//     }
+//     if (filter === "completed") {
+//         tasksForTodolist = tasks.filter(t => t.isDone === true);
+//     }
+//
+//     function changeFilter(value: FilterValuesType) {
+//         setFilter(value);
+//     }
+//
+//
+//     return (
+//         <div className="App">
+//             <Todolist title="What to learn"
+//                       tasks={tasksForTodolist}
+//                       removeTask={removeTask}
+//                       changeFilter={changeFilter}
+//                       addTask={addTask}>
+//                 <div>
+//                     <div>Many intresting information</div>
+//                     <div>Many intresting information</div>
+//                     <div>Many intresting information</div>
+//                     <div>Many intresting information</div>
+//                     <div>Many intresting information</div>
+//                 </div>
+//             </Todolist>
+//
+//             <Todolist title="What to learn"
+//                       tasks={tasksForTodolist}
+//                       removeTask={removeTask}
+//                       changeFilter={changeFilter}
+//                       addTask={addTask}>
+//                 <div>
+//                     <div>A lot of boring information</div>
+//                     <div>A lot of boring information</div>
+//                     <div>A lot of boring information</div>
+//                     <input placeholder={'A lot of boring information'}/>
+//                     <div>
+//                         <button>Boring Button 1</button>
+//                         <button>Boring Button 2</button>
+//                         <button>Boring Button 3</button>
+//                     </div>
+//                 </div>
+//             </Todolist>
+//
+//             <Todolist title="What to learn"
+//                       tasks={tasksForTodolist}
+//                       removeTask={removeTask}
+//                       changeFilter={changeFilter}
+//                       addTask={addTask}/>
+//         </div>
+//     );
+// }
+//
+// export default App;
